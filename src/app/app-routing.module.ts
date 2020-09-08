254d9e626af36bca0995d29652cc5d8dc3a4b966
dev <dev@kierans-portfolio.attlocal.net>
2020-09-08 15:41:12 -0500

    Route to the login, logout, and register components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// CRUD components
 import { UsersComponent }   from './users/users.component';
 import { ArticlesComponent } from './articles/articles.component';
 import { UserViewComponent } from './user-view/user-view.component';
@@ -8,14 +9,23 @@ import { UserCreateComponent } from './user-create/user-create.component';
 import { ArticleCreateComponent } from './article-create/article-create.component';
 import { UserEditComponent }   from './user-edit/user-edit.component';
 import { ArticleEditComponent }   from './article-edit/article-edit.component';
-
+// AUTH components
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/users', pathMatch: 'full'},
+  { path: '', redirectTo: '/login', pathMatch: 'full'},
+  // AUTH
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: 'register', component: RegisterComponent },
+  // USERS
   { path: 'users', component: UsersComponent },
   { path: 'users/view/:id', component: UserViewComponent },
   { path: 'users/create', component: UserCreateComponent },
   { path: 'users/edit/:id', component: UserEditComponent },
+  // ARTICLES
   { path: 'articles', component: ArticlesComponent },
   { path: 'articles/view/:id', component: ArticleViewComponent },
   { path: 'articles/create', component: ArticleCreateComponent },
